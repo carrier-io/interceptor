f554ae19f989a0daab24bd1260dc3a8e81ebbb69
arozumenko <artyom.rozumenko@gmail.com>
2018-12-12 21:20:10 +0200

    added License Disclaimers
    Signed-off-by: arozumenko <artyom.rozumenko@gmail.com>

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
-#  Copyright (c) 2018 EPAM Systems, Inc.
+#   Copyright 2018 getcarrier.io
 #
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#       http://www.apache.org/licenses/LICENSE-2.0
 #
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 
 from setuptools import setup, find_packages
 
@@ -22,7 +23,7 @@ setup(
     version='1.0.0',
     description='Core component',
     long_description='Celery worker to execute various types of workloads',
-    url='https://epam.com',
+    url='https://getcarrier.io',
     license='Apache License 2.0',
     author='arozumenko',
     author_email='artem_rozumenko@epam.com',
