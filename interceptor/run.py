0e047fedbfb06d71ba64ae6265e13b3e2fdfcf64
arozumenko <artyom.rozumenko@gmail.com>
2019-05-27 18:56:30 +0300

    added print for resource usage
    Signed-off-by: arozumenko <artyom.rozumenko@gmail.com>

--- interceptor/run.py
+++ interceptor/run.py
@@ -12,8 +12,11 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+import docker
+
 from os import environ
 from time import sleep
+from interceptor.constants import CPU_MULTIPLIER
 from celery import Celery
 from celery.contrib.abortable import AbortableTask
 
@@ -39,15 +42,23 @@ app.conf.update(timezone='UTC', result_expires=1800)
 def execute_job(self, job_type, container, execution_params, redis_connection, job_name, *args, **kwargs):
     if not getattr(JobsWrapper, job_type):
         return False, "Job Type not found"
-    cid = getattr(JobsWrapper, job_type)(container, execution_params, job_name, redis_connection,
+    client = docker.from_env()
+    client.info()
+    cid = getattr(JobsWrapper, job_type)(client, container, execution_params, job_name, redis_connection,
                                          *args, **kwargs)
+    client_lowlevel = docker.APIClient(base_url='unix://var/run/docker.sock')
     while cid.status != "exited":
         if self.is_aborted():
             cid.stop(timeout=60)
             return True, "Aborted"
         try:
             cid.reload()
-            print(cid.status)
+            print(f'Container Status: {cid.status}')
+            resource_usage = client_lowlevel.stats(cid.id, stream=False)
+            print(f'Container {cid.id} resource usage -- '
+                  f'CPU: {round(float(resource_usage["cpu_stats"]["cpu_usage"]["total_usage"])/CPU_MULTIPLIER, 2)} '
+                  f'RAM: {round(float(resource_usage["memory_stats"]["usage"])/(1024*1024), 2)} Mb '
+                  f'of {round(float(resource_usage["memory_stats"]["limit"])/(1024*1024), 2)} Mb')
         except:
             break
         sleep(10)
