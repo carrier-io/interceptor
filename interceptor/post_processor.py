75d58efa8bec6c7be2727fbf2078493e620876ba
Mykhailo Hunko <mykhailo_hunko@epam.com>
2020-03-11 15:45:04 +0200

    Update post_process task signature

--- interceptor/post_processor.py
+++ interceptor/post_processor.py
@@ -5,12 +5,13 @@ import requests
 
 class PostProcessor:
 
-    def __init__(self, galloper_url, galloper_web_hook, bucket, prefix, *args, **kwargs):
+    def __init__(self, galloper_url, galloper_web_hook, bucket, prefix, junit=False, *args, **kwargs):
         self.galloper_url = galloper_url
         self.galloper_web_hook = galloper_web_hook
         self.bucket = bucket
         self.prefix = prefix
         self.config_file = '{}'
+        self.junit = junit
 
     def results_post_processing(self):
         if self.galloper_web_hook:
@@ -21,7 +22,7 @@ class PostProcessor:
                 self.config_file = environ.get('CONFIG_FILE', '{}')
 
             data = {'galloper_url': self.galloper_url, 'config_file': json.dumps(self.config_file),
-                    'bucket': self.bucket, 'prefix': self.prefix}
+                    'bucket': self.bucket, 'prefix': self.prefix, "junit": self.junit}
             headers = {'content-type': 'application/json'}
             r = requests.post(self.galloper_web_hook, json=data, headers=headers)
             print(r.text)
