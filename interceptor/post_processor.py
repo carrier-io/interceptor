c1ccdbc79fcfa3320bd39e799242cbbcead332f6
Mykhailo Hunko <mykhailo_hunko@epam.com>
2020-03-30 18:51:56 +0300

    Added mounts to free_style job

--- interceptor/post_processor.py
+++ interceptor/post_processor.py
@@ -15,18 +15,19 @@ class PostProcessor:
         self.junit = junit
 
     def results_post_processing(self):
-        if path.exists('/tmp/config.yaml'):
-            with open("/tmp/config.yaml", "r") as f:
-                self.config_file = f.read()
-        else:
-            self.config_file = environ.get('CONFIG_FILE', '{}')
+        if self.galloper_web_hook:
+            if path.exists('/tmp/config.yaml'):
+                with open("/tmp/config.yaml", "r") as f:
+                    self.config_file = f.read()
+            else:
+                self.config_file = environ.get('CONFIG_FILE', '{}')
 
-        data = {'galloper_url': self.galloper_url, 'project_id': self.project_id,
-                'config_file': json.dumps(self.config_file),
-                'bucket': self.bucket, 'prefix': self.prefix, "junit": self.junit}
-        headers = {'content-type': 'application/json'}
-        r = requests.post(self.galloper_web_hook, json=data, headers=headers)
-        print(r.text)
+            data = {'galloper_url': self.galloper_url, 'project_id': self.project_id,
+                    'config_file': json.dumps(self.config_file),
+                    'bucket': self.bucket, 'prefix': self.prefix, "junit": self.junit}
+            headers = {'content-type': 'application/json'}
+            r = requests.post(self.galloper_web_hook, json=data, headers=headers)
+            print(r.text)
 
 
 
