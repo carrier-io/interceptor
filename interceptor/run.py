348a50633420f28450d9cc4250d0b4252b7fd116
Mykhailo Hunko <mykhailo_hunko@epam.com>
2020-04-13 15:25:30 +0300

    Added AOToken

--- interceptor/run.py
+++ interceptor/run.py
@@ -45,9 +45,9 @@ app.conf.update(
 
 @app.task(name="tasks.post_process", bind=True, acks_late=False)
 def post_process(self, results, galloper_url, project_id, galloper_web_hook,
-                 bucket, prefix, junit=False, *args, **kwargs):
+                 bucket, prefix, junit=False, token=None, *args, **kwargs):
     return PostProcessor(galloper_url, project_id, galloper_web_hook, bucket,
-                         prefix, junit).results_post_processing()
+                         prefix, junit, token).results_post_processing()
 
 
 @app.task(name="tasks.execute", bind=True, acks_late=True, base=AbortableTask)
