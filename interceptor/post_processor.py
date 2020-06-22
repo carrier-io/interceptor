6edc5317550919ec55327013b4e93aa82321cd2b
mykhailo_hunko <mykhailo.hunko@epam.com>
2020-06-22 15:17:36 +0300

    Added integrations

--- interceptor/post_processor.py
+++ interceptor/post_processor.py
@@ -6,7 +6,7 @@ import requests
 class PostProcessor:
 
     def __init__(self, galloper_url, project_id, galloper_web_hook, bucket, prefix, junit=False, token=None,
-                 jira= False, *args, **kwargs):
+                 integration=[], email_recipients=None, *args, **kwargs):
         self.galloper_url = galloper_url
         self.project_id = project_id
         self.galloper_web_hook = galloper_web_hook
@@ -15,7 +15,8 @@ class PostProcessor:
         self.config_file = '{}'
         self.junit = junit
         self.token = token
-        self.jira = jira
+        self.integration = integration
+        self.email_recipients = email_recipients
 
     def results_post_processing(self):
         if self.galloper_web_hook:
@@ -28,7 +29,7 @@ class PostProcessor:
             data = {'galloper_url': self.galloper_url, 'project_id': self.project_id,
                     'config_file': json.dumps(self.config_file),
                     'bucket': self.bucket, 'prefix': self.prefix, 'junit': self.junit, 'token': self.token,
-                    'jira': self.jira}
+                    'integration': self.integration, "email_recipients": self.email_recipients}
             headers = {'content-type': 'application/json'}
             if self.token:
                 headers['Authorization'] = f'bearer {self.token}'
