10ef2edf1b4a0d00391a7c1efb30808435b8dfb4
Mykhailo_Hunko <mykhailo.hunko@gmail.com>
2024-09-03 18:40:43 +0300

    remove comments

--- interceptor/constants.py
+++ interceptor/constants.py
@@ -26,7 +26,7 @@ REMOVE_CONTAINERS = True if environ.get("REMOVE_CONTAINERS", "True") == "True" e
 PYLON_URL = environ.get('PYLON_URL')
 LAMBDA_DOCKER_NETWORK = environ.get("LAMBDA_DOCKER_NETWORK", "").strip()
 LAMBDA_DOCKER_NETWORK_MODE = environ.get("LAMBDA_DOCKER_NETWORK_MODE", "").strip()
-EXTRA_HOSTS = environ.get("EXTRA_HOSTS", "").strip() #{'stagev2.palmettostatearmory.com': '10.237.20.73'}
+EXTRA_HOSTS = environ.get("EXTRA_HOSTS", "").strip()
 
 
 NAME_CONTAINER_MAPPING = {
